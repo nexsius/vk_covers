39ef844ed3b43f812c6d79f6dbcf8e8f72304c3e
nexs <nexsius@gmail.com>
2017-11-15 09:31:21 +0300

    build please

--- src/main/java/servlets/lastuser.java
+++ src/main/java/servlets/lastuser.java
@@ -78,7 +78,7 @@ public class lastuser {
                g.setStroke(new BasicStroke(8));
 
 
-               String query = "select date_format(a.date, '%d.%m'), a.place, a.descr_short from afisha a join bands b on (a.band_id = b.band_id) where b.band_name='Марко Поло'";
+               String query = "select date_format(a.date, '%d.%m'), a.place, a.descr_short from afisha a where a.band_id=156838463";
 
                try {
                    // opening database connection to MySQL server
